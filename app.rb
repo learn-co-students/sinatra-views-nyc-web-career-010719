14123b7a38fb4f971dcc734959bd82a3941664a0
Sarah Shin <sarahsujin.shin@gmail.com>
2019-01-28 12:58:00 -0500

    Done.

--- app.rb
+++ app.rb
@@ -2,7 +2,20 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+	# get '/' do
+	# 	"<h1>Hello World</hi>"
+	# end
+
 	get '/' do
-		
+	  erb :index
 	end
+
+	# get "/info" do
+	#   "Testing the info page"
+	# end
+
+	get "/info" do
+		erb :info
+	end
+
 end
